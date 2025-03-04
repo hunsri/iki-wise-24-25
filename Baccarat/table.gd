046b893e72680809f515f1a92738757683ca3511
s-fx <fxscholler@protonmail.com>
2025-03-04 10:52:07 +0100

    add score

--- Baccarat/table.gd
+++ Baccarat/table.gd
@@ -15,6 +15,10 @@ var rank_index = 0
 @onready var player_hand: CardCollection3D = $Player_Hand
 @onready var dealer_hand: CardCollection3D = $Dealer_Hand
 
+@onready var player_score_label: Label = $Player_Score
+@onready var dealer_score_label: Label = $Dealer_Score
+@onready var winner_label: Label = $Winner
+
 
 func next_card():
 	# Hier muss noch random gezogen werden
@@ -109,54 +113,92 @@ func _on_button_play_pressed():
 	# Remove cards before each round
 	player_hand.remove_all()
 	dealer_hand.remove_all()
+	# Remove labels
+	winner_label.text = ""
+	player_score_label.text = ""
+	dealer_score_label.text = ""
 	
+	# Give Player and Dealer two cards
 	get_2_card(player_hand)
 	get_2_card(dealer_hand)
 	print("Player has: ", player_hand.cards)
 	print("Dealer has: ", dealer_hand.cards)
+	
+	# Calculate the current score for the player and dealer
 	var player_score = calc_score(player_hand)
+	player_score_label.text = "Player Score: " + str(player_score)
 	var dealer_score = calc_score(dealer_hand)
+	dealer_score_label.text = "Dealer Score: " + str(dealer_score)
 	print("Player Score: ", player_score)
 	print("Dealer Score: ", dealer_score)
+	
+	# Check if there is already a winner
 	var winner = check_if_winner(player_score, dealer_score)  
 	print("Has player won? -> ", winner[0])
 	print("Has dealer won? -> ", winner[1])
 	print("Result: ", winner[2])
-	# Get 3. card
+	
+	# If there is no winner yet do:
 	if (winner[2] == "No winner yet"):
+		# Check if the player takes a third card according to the rules
 		var player_third_card = check_player_third_card(player_score, dealer_score)
+		# If the player has to get a third card do:
 		if (player_third_card):
 			print("Player gets 3. card")
 			# Player takes 3. card and wait 2 seconds for animation
 			await get_tree().create_timer(2).timeout 
 			var player_3_card_rank = get_card(player_hand)
-			print("Player took 3. card")
+			# Check new score 
+			player_score = calc_score(player_hand)
+			player_score_label.text = "Player Score: " + str(player_score)
+			# Check if dealer also gets a third card according to the rule
 			if (dealer_score >= 0 && dealer_score <= 2):
 				print("Dealer gets also a 3. card")
 				await get_tree().create_timer(2).timeout 
 				var dealer_card_rank = get_card(dealer_hand)
+				dealer_score = calc_score(dealer_hand)
+				dealer_score_label.text = "Dealer Score: " + str(dealer_score)
 			elif (dealer_score >= 3 && dealer_score <= 6):
 				if (player_3_card_rank != 8):
 					print("Dealer gets also a 3. card")
 					await get_tree().create_timer(2).timeout 
 					var dealer_card_rank = get_card(dealer_hand)
+					dealer_score = calc_score(dealer_hand)
+					dealer_score_label.text = "Dealer Score: " + str(dealer_score)
 		elif not player_third_card:
 			if (dealer_score >= 0 && dealer_score <= 5):
 				print("Dealer gets 3. card")
 				await get_tree().create_timer(2).timeout 
 				var dealer_card_rank = get_card(dealer_hand)
+				dealer_score = calc_score(dealer_hand)
+				dealer_score_label.text = "Dealer Score: " + str(dealer_score)
 			elif (dealer_score >= 6 && dealer_score <= 7):
 				print("Dealer stands. No 3. card for dealer")
 				
 		# Check score again, highest score wins
 		player_score = calc_score(player_hand)
+		player_score_label.text = "Player Score: " + str(player_score)
 		dealer_score = calc_score(dealer_hand)
+		dealer_score_label.text = "Dealer Score: " + str(dealer_score)
 		print("Player Score: ", player_score)
 		print("Dealer Score: ", dealer_score)
+		
 		if player_score > dealer_score:
 			print("Player wins")
+			winner_label.text = "--> PLAYER WINS <--"
 		else:
 			print("Dealer wins")
+			winner_label.text = "--> DEALER WINS <--"
+	# if it is a tie
+	elif (winner[2] == "tie"):
+		winner_label.text = "--> IT IS A TIE <--"
+	elif (winner[2] == "player wins"):
+		winner_label.text = "--> PLAYER WINS <--"
+	elif (winner[2] == "dealer wins"):
+		winner_label.text = "--> DEALER WINS <--"
+	else:
+		print("Shit da klappt was nicht")
+	
 	print("End of round!")
 	print("--------------------------")
 		
@@ -173,4 +215,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
