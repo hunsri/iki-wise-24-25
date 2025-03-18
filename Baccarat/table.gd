bf8e2769eeae1d6632422c06726367aae75987c5
s-fx <fxscholler@protonmail.com>
2025-03-18 15:39:47 +0100

    blackjack

--- Baccarat/table.gd
+++ Baccarat/table.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
-# 52 per deck, 4 decks are used for Baccarat
-const DECK_SIZE = 52 * 4
+# 52 per deck, 1 deck is used for Blackjack
+const DECK_SIZE = 52 * 1
 
 var card_database = FaceCards.new()
 var suits = [
@@ -25,6 +25,11 @@ var round_ongoing:bool = false
 @onready var player_score_label: Label = $Player_Score
 @onready var dealer_score_label: Label = $Dealer_Score
 @onready var winner_label: Label = $Winner
+
+@onready var info_label: Label = $Info_label
+
+# Asks the player if he wants to take a 3. card
+@export var cardDialog : CardDialog
 
 func _ready() -> void:
 	fill_deck()
@@ -79,72 +84,65 @@ func fill_deck():
 		deck.append_card(card)
 	
 	deck.cards.shuffle()
-
-func calc_score(hand) -> int:
+	
+	
+# NOT FINISHED YET
+func calc_score(hand):
 	var score = 0
+	var ace_found = false
+	
 	for card in hand.cards:
-		# In case it is an Ace
-		if card.rank == 14:
-			score = score + 1
-		# In case it is 10, B, D, K
-		elif card.rank >= 10:
-			score = score + 0
-		else:
-			score = score + card.rank
-	# Finally check if score > 9 and drop first digit
-	if score > 9:
-		score = score % 10
+		if card.rank <= 10:
+			score += card.rank
+		elif (card.rank >= 10 && card.rank <= 13):
+			score += 10
+		# NOT FINISHED YET
+		elif card.rank == 14:
+			ace_found = true
+	if score < 12 && ace_found:
+		score += 10
+	elif score >= 12 && ace_found:
+		score += 1
 	return score
-	
-	
-func check_if_winner(player_score, dealer_score):
-	if (player_score == 9 && dealer_score == 9):
-		return [true, true, 'tie']
-	if (player_score == 8 && dealer_score == 8):
-		return [true, true, 'tie']
-	
-	if (player_score == 9 && dealer_score == 8):
-		return [true, false, 'player wins']
-	if (player_score == 8 && dealer_score == 9):
-		return [false, true, 'dealer wins'] 
 
-	if (player_score == 8 || player_score == 9):
-		if (dealer_score < 8):
-			return [true, false, 'player wins']
-	if (dealer_score == 8 || dealer_score == 9):
-		if (player_score < 8):
-			return [false, true, 'dealer wins']
+# Check if dealer gets a 3. card	
+func dealer_3card(hand, score):
+	if score <= 16:
+		get_card(hand)
+		info_label.text += "Dealer takes 3. card\n"
 	
-	if (player_score < 8 && dealer_score < 8):
-		return [false, false, "No winner yet"]
 
-func check_player_third_card(player_score, dealer_score):
-	var player_third = false
-	# Player "stands" if between 6-7 -> then player gets no third card
-	if (player_score == 6 || player_score == 7):
-		player_third = false
-	# Player gets 3. card if between 0-5
-	if (player_score >= 0 && player_score <= 5):
-		player_third = true
-	return player_third
-
-func should_bank_get_third_card(player_third_card: int, dealer_score: int) -> bool:
-
-	# Check if dealer also gets a third card according to the rule
-	if (dealer_score >= 0 && dealer_score <= 2):
-		return true
-	elif (dealer_score == 3 && player_third_card != 8):
-		return true
-	elif (dealer_score == 4 && player_third_card >= 2 && player_third_card <= 7 ):
-		return true
-	elif (dealer_score == 5 && player_third_card >= 4 && player_third_card <= 7 ):
-		return true
-	elif (dealer_score == 6 && player_third_card >= 6 && player_third_card <= 7 ):
-		return true
+func display_score(player_score, dealer_score):
+	player_score_label.text = "Player Score: " + str(player_score)
+	dealer_score_label.text = "Dealer Score: " + str(dealer_score)
 	
+
+func check_blackjack(score):
+	if score == 21:
+		info_label.text += "Blackjack!\n"
+		return true
+	return false
+
+func check_lost(score):
+	if score > 21:
+		return true
 	return false
 
 func _on_button_play_pressed():
+	
+	# [0] Set up labels, scores, hands and deck
+	var player_score = 0
+	var dealer_score = 0
+	var player_wins = false
+	var dealer_wins = false
+	info_label.text = "[Info]\n"
+	# Remove cards before each round
+	player_hand.remove_all()
+	dealer_hand.remove_all()
+	# Remove labels
+	winner_label.text = ""
+	player_score_label.text = ""
+	dealer_score_label.text = ""
 	
 	if stop_index_reached():
 		print("++++++++++++++++++++++")
@@ -157,91 +155,77 @@ func _on_button_play_pressed():
 	else:
 		round_ongoing = true
 	
-	print("Play one Round of Baccarat")
-	# Remove cards before each round
-	player_hand.remove_all()
-	dealer_hand.remove_all()
-	# Remove labels
-	winner_label.text = ""
-	player_score_label.text = ""
-	dealer_score_label.text = ""
+	# [1] START OF ROUND
+	print("Play one Round of Blackjack")
+	info_label.text += "Play one round of Blackjack\n"
 	
-	# Give Player and Dealer two cards
+	# [2] Give Player and Dealer two cards
+	# TODO: Dont show the first card of the dealer
 	get_2_card(player_hand)
 	get_2_card(dealer_hand)
-	
 	print("Player has: ", player_hand.cards)
 	print("Dealer has: ", dealer_hand.cards)
+	# Calculate current score
+	player_score = calc_score(player_hand)
+	dealer_score = calc_score(dealer_hand)
+	display_score(player_score, dealer_score)
 	
-	# Calculate the current score for the player and dealer
-	var player_score = calc_score(player_hand)
-	player_score_label.text = "Player Score: " + str(player_score)
-	var dealer_score = calc_score(dealer_hand)
-	dealer_score_label.text = "Dealer Score: " + str(dealer_score)
-	print("Player Score: ", player_score)
-	print("Dealer Score: ", dealer_score)
+	# [2.5] Check for blackjack
+	player_wins = check_blackjack(player_score)
+	dealer_wins = check_blackjack(dealer_score)
 	
-	# Check if there is already a winner
-	var winner = check_if_winner(player_score, dealer_score)  
-	print("Has player won? -> ", winner[0])
-	print("Has dealer won? -> ", winner[1])
-	print("Result: ", winner[2])
-	
-	# If there is no winner yet do:
-	if (winner[2] == "No winner yet"):
-		# Check if the player takes a third card according to the rules
-		var player_third_card = check_player_third_card(player_score, dealer_score)
-		# If the player has to get a third card do:
-		if (player_third_card):
-			print("Player gets 3rd card")
-			# Player takes 3. card and wait 2 seconds for animation
-			await get_tree().create_timer(2).timeout 
-			var player_3_card_rank = get_card(player_hand)
-			# Check new score 
-			player_score = calc_score(player_hand)
-			player_score_label.text = "Player Score: " + str(player_score)
-			
-			if should_bank_get_third_card(player_3_card_rank, dealer_score):
-				print("Dealer gets also a 3rd card")
-				await get_tree().create_timer(2).timeout 
-				var dealer_card_rank = get_card(dealer_hand)
-				dealer_score = calc_score(dealer_hand)
-				dealer_score_label.text = "Dealer Score: " + str(dealer_score)
-				
-		elif not player_third_card:
-			if (dealer_score >= 0 && dealer_score <= 5):
-				print("Dealer gets 3rd card")
-				await get_tree().create_timer(2).timeout 
-				var dealer_card_rank = get_card(dealer_hand)
-				dealer_score = calc_score(dealer_hand)
-				dealer_score_label.text = "Dealer Score: " + str(dealer_score)
-			elif (dealer_score >= 6 && dealer_score <= 7):
-				print("Dealer stands. No third card for dealer")
-				
-		# Check score again, highest score wins
-		player_score = calc_score(player_hand)
-		player_score_label.text = "Player Score: " + str(player_score)
-		dealer_score = calc_score(dealer_hand)
-		dealer_score_label.text = "Dealer Score: " + str(dealer_score)
-		print("Player Score: ", player_score)
-		print("Dealer Score: ", dealer_score)
-		
-		if player_score > dealer_score:
-			print("Player wins")
-			winner_label.text = "--> PLAYER WINS <--"
-		else:
-			print("Dealer wins")
-			winner_label.text = "--> DEALER WINS <--"
-	# if it is a tie
-	elif (winner[2] == "tie"):
-		winner_label.text = "--> IT IS A TIE <--"
-	elif (winner[2] == "player wins"):
-		winner_label.text = "--> PLAYER WINS <--"
-	elif (winner[2] == "dealer wins"):
-		winner_label.text = "--> DEALER WINS <--"
+	if (player_wins && not dealer_wins):
+		round_ongoing = false
+		info_label.text += "[Result] Player wins\n"
 	else:
-		print("Shit da klappt was nicht")
+		# [3] Ask player for 3. card and wait for button pressed
+		cardDialog.popup()
+		var take_3_card = await cardDialog.wait_for_user_decision()
+		if take_3_card:
+			get_card(player_hand)
+
+		# Recalculate the score
+		player_score = calc_score(player_hand)
+		display_score(player_score, dealer_score)
+		print("Player has: ", player_hand.cards)
+		info_label.text += "Player cards: " + str(player_hand.cards) + "\n"
+		info_label.text += "Dealer cards: " + str(dealer_hand.cards) + "\n"
+		
+		# [3.5] Check if player lost 
+		if check_lost(player_score):
+			info_label.text += "[Result] Dealer wins\n"
+		# If not continue
+		else:
+			# TODO: Implement a function that reveals the 2. card of the dealer
+			# and recalculates the score
+			# [4] Check if dealer gets a 3. card and recalculate the score
+			dealer_3card(dealer_hand, dealer_score)
+			dealer_score = calc_score(dealer_hand)
+			display_score(player_score, dealer_score)
+		
+			# [4.5] Check for blackjack
+			player_wins = check_blackjack(player_score)
+			dealer_wins = check_blackjack(dealer_score)
+			if player_wins && not dealer_wins:
+				info_label.text += "[Result] Player wins\n"
+			elif player_wins && dealer_wins:
+				info_label.text += "[Result] Tie\n"
+			elif dealer_wins && not player_wins:
+				info_label.text += "[Result] Dealer wins\n"
+			# No blackjack, calculating the score
+			else:
+				if check_lost(dealer_score):
+					info_label.text += "[Result] Player wins\n"
+				elif player_score > dealer_score:
+					info_label.text += "[Result] Player wins\n"
+				elif dealer_score > player_score:
+					info_label.text += "[Result] Dealer wins\n"
+				elif dealer_score == player_score:
+					info_label.text += "[Result] Tie\n"
+		
 	
+	# End of round
+	info_label.text += "End of Round\n"
 	print("End of round!")
 	print("--------------------------")
 	
